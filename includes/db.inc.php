1430566351bd407ff2d7de5b80ecf1f2d13bc4f9
syedaaiman16 <113286124+syedaaiman16@users.noreply.github.com>
2023-07-18 23:15:23 +0500

    Add files via upload

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,1 +1,10 @@
+<?php
 
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "school";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
